afcd1a6e58ac7de6a0bc974185bf2d922c4fe0de
heitzjulien <heitzjulien@pm.me>
2024-06-17 16:02:19 +0200

    fix: config material tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,24 @@
-import styled from "@emotion/styled";
-import { Box, CssBaseline, Toolbar } from "@mui/material";
+// import styled from "@emotion/styled";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import { TopMenu } from "./TopMenu";
-import { Dashboard } from "./dashboard/Dashboard";
-import { AppTheme } from "./Theme";
+import "./index.css";
+import Header from "./components/Header.tsx"
+import Footer from "./components/Footer.tsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "offres",
-        element: <Box>Offres</Box>,
-      },
-      {
-        path: "parametres",
-        element: <Box>Paramètres</Box>,
-      },
-      {
-        path: "selection",
-        element: <Box>Ma sélection</Box>,
-      },
     ],
   },
 ]);
-const MainBox = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
 
 function Layout() {
   return (
-    <AppTheme>
-      <MainBox>
-        <CssBaseline />
-        <TopMenu />
-        <Box component="main">
-          <Toolbar />
-          <Outlet />
-        </Box>
-      </MainBox>
-    </AppTheme>
+    <>
+        <Header />
+        <Footer />
+    </>
   );
 }
 
--- frontend/src/auth/Profile.tsx
+++ frontend/src/auth/Profile.tsx
@@ -1,73 +1,73 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import {
-  Avatar,
-  Button,
-  List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-  Popover,
-} from "@mui/material";
-import LogoutIcon from "@mui/icons-material/Logout";
-import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import {
+//   Avatar,
+//   Button,
+//   List,
+//   ListItemButton,
+//   ListItemIcon,
+//   ListItemText,
+//   ListSubheader,
+//   Popover,
+// } from "@mui/material";
+// import LogoutIcon from "@mui/icons-material/Logout";
+// import React from "react";
 
-function getInitialFromUserName(name: string) {
-  const words = name.split(" ");
-  if (words.length === 1) {
-    return `${name[0]}${name[1]}`;
-  } else {
-    return `${words[0][0]}${words[1][0]}`;
-  }
-}
+// function getInitialFromUserName(name: string) {
+//   const words = name.split(" ");
+//   if (words.length === 1) {
+//     return `${name[0]}${name[1]}`;
+//   } else {
+//     return `${words[0][0]}${words[1][0]}`;
+//   }
+// }
 
-export function Profile() {
-  const { user, logout } = useAuth0();
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
-    null
-  );
+// export function Profile() {
+//   const { user, logout } = useAuth0();
+//   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+//     null
+//   );
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+//   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+//     setAnchorEl(event.currentTarget);
+//   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
 
-  const popoverIsOpen = Boolean(anchorEl);
-  const name = user?.name || "Anonymous";
+//   const popoverIsOpen = Boolean(anchorEl);
+//   const name = user?.name || "Anonymous";
 
-  return (
-    <div>
-      <Button onClick={handleClick}>
-        <Avatar>{getInitialFromUserName(name)}</Avatar>
-      </Button>
-      <Popover
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        onClose={handleClose}
-        open={popoverIsOpen}
-      >
-        <List
-          sx={{ bgcolor: "background.paper" }}
-          subheader={<ListSubheader>{name}</ListSubheader>}
-        >
-          <ListItemButton onClick={() => logout()}>
-            <ListItemIcon>
-              <LogoutIcon />
-            </ListItemIcon>
-            <ListItemText primary="Déconnexion" />
-          </ListItemButton>
-        </List>
-      </Popover>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Button onClick={handleClick}>
+//         <Avatar>{getInitialFromUserName(name)}</Avatar>
+//       </Button>
+//       <Popover
+//         anchorEl={anchorEl}
+//         anchorOrigin={{
+//           vertical: "bottom",
+//           horizontal: "center",
+//         }}
+//         transformOrigin={{
+//           vertical: "top",
+//           horizontal: "center",
+//         }}
+//         onClose={handleClose}
+//         open={popoverIsOpen}
+//       >
+//         <List
+//           sx={{ bgcolor: "background.paper" }}
+//           subheader={<ListSubheader>{name}</ListSubheader>}
+//         >
+//           <ListItemButton onClick={() => logout()}>
+//             <ListItemIcon>
+//               <LogoutIcon />
+//             </ListItemIcon>
+//             <ListItemText primary="Déconnexion" />
+//           </ListItemButton>
+//         </List>
+//       </Popover>
+//     </div>
+//   );
+// }
