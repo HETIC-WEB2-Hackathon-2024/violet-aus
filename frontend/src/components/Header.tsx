bb7e3fa323da6ba7c40781c93dca09141cfa3640
Nyoote <faustinecharrier@gmail.com>
2024-06-18 13:14:48 +0200

    feat: add sidenav state

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,199 +5,118 @@ import {
   List,
   ListItem,
   ListItemPrefix,
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
 } from "@material-tailwind/react";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
-export default function Header() {
-  const [open, setOpen] = React.useState(0);
-  // const [sidenavOpen, setSidenavOpen] = useState(true);
+const Menus = [
+  { title: "Dashboard", icon: "Presentation_Chart", link: "/Dashboard" },
+  { title: "Offres", icon: "Briefcase", link: "/Offres" },
+  { title: "Ma sélection", icon: "Bookmark", link: "/Selection" },
+  { title: "Paramètres", icon: "Params", link: "/Parametres" },
+  { title: "Connexion", icon: "Connexion", link: "/Connexion" },
 
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
-  };
+];
+
+const icons = {
+  Presentation_Chart:
+    "M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6",
+  Briefcase:
+    "M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z",
+  Bookmark:
+    "M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z",
+  Params:
+  "M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z",
+  Connexion:
+  "M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9",
+
+};
+
+
+
+export default function Header() {
+  const [sidenavOpen, setSidenavOpen] = React.useState(true);
   return (
-    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl bg-primary-base rounded-none">
+    <Card
+      className={` ${
+        sidenavOpen ? "w-72" : "w-20 "
+      } relative duration-300 h-100vh p-1 shadow-xl bg-primary-base rounded-none`}
+    >
       <Link to="/">
-        <div className="mb-2 flex items-center gap-4 p-4">
-          <img src="#" alt="brand" className="h-8 w-8" />
-          <Typography variant="h5" color="white">
+        <ListItemPrefix className="text-secondary-light">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className={`absolute cursor-pointer -right-3 top-9 w-7 border-secondary-light
+                         border-2 rounded-full size-6  ${
+                           !sidenavOpen && "rotate-180"
+                         }`}
+            onClick={() => setSidenavOpen(!sidenavOpen)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+        </ListItemPrefix>
+        <div className="mb-2 flex flex-col items-center gap-2 p-4">
+          <img
+            src="https://www.stickers-garage.com/imgtransform.php?f=1&filename=https://www.stickers-garage.com/resize/720x720/zc/3/f/0/src/sites/stickers/files/products/429f5d5147a6874bc1fd2d867927305a.png"
+            className={`cursor-pointer duration-500 ${
+              sidenavOpen && "rotate-[360deg]"
+            }`}
+          />
+          <Typography
+            variant="h5"
+            color="white"
+            className={`${
+              !sidenavOpen && "hidden"
+            } origin-left duration-200 text-center`}
+          >
             AUS
           </Typography>
         </div>
       </Link>
+      <hr className=" border-primary-dark" />
       <List>
-        <hr className="my-2 border-primary-dark" />
-        <Link to="/Dashboard">
-          <ListItem className="hover:bg-primary-dark focus:bg-primary-light">
-            <ListItemPrefix className="text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"
-                />
-              </svg>
-            </ListItemPrefix>
-            <Typography variant="h5" color="white">
-              Dashboard
-            </Typography>
-          </ListItem>
-        </Link>
-        <Link to="/Offres">
-          <ListItem className="hover:bg-primary-dark focus:bg-primary-light">
-            <ListItemPrefix className="text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
-                />
-              </svg>
-            </ListItemPrefix>
-            <Typography variant="h5" color="white">
-              Offres
-            </Typography>
-          </ListItem>
-        </Link>
-
-        <Link to="/Selection">
-          <ListItem className="hover:bg-primary-dark focus:bg-primary-light">
-            <ListItemPrefix className="text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-                />
-              </svg>
-            </ListItemPrefix>
-            <Typography variant="h5" color="white">
-              Ma sélection
-            </Typography>
-          </ListItem>
-        </Link>
-        <Accordion
-          open={open === 1}
-          icon={
-            <ChevronDownIcon
-              strokeWidth={2.5}
-              className={`mx-auto h-4 w-4 transition-transform text-white ${
-                open === 1 ? "rotate-180" : ""
-              }`}
-            />
-          }
-        >
-          <ListItem className="hover:bg-primary-dark p-0" selected={open === 1}>
-            <AccordionHeader
-              onClick={() => handleOpen(1)}
-              className="border-b-0 p-3 focus:bg-primary-light"
+        {Menus.map((menu, index) => (
+          <Link key={index} to={menu.link}>
+            <ListItem
+              className={` ${
+                sidenavOpen ? "w-70" : "w-12"
+              } relative duration-300 hover:bg-primary-dark focus:bg-primary-light`}
             >
               <ListItemPrefix className="text-white">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="size-6"
+                  className="size-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d={icons[menu.icon]}
                   />
                 </svg>
               </ListItemPrefix>
               <Typography
-                color="white"
-                className="mr-auto font-normal"
                 variant="h5"
+                color="white"
+                className={`${
+                  !sidenavOpen && "hidden"
+                } origin-left duration-200 ml-2`}
               >
-                Profil
+                {menu.title}
               </Typography>
-            </AccordionHeader>
-          </ListItem>
-          <AccordionBody className="py-1 ml-2 w-1">
-            <List className="p-0">
-              <Link to="/Parametres">
-                <ListItem className="hover:bg-primary-dark focus:bg-primary-light">
-                  <ListItemPrefix className="text-white">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                      />
-                    </svg>
-                  </ListItemPrefix>
-                  <Typography variant="h5" color="white">
-                    Paramètres
-                  </Typography>
-                </ListItem>
-              </Link>
-              <Link to="/Connexion">
-                <ListItem className="hover:bg-primary-dark focus:bg-primary-light">
-                  <ListItemPrefix className="text-white">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9"
-                      />
-                    </svg>
-                  </ListItemPrefix>
-                  <Typography variant="h5" color="white">
-                    Connexion
-                  </Typography>
-                </ListItem>
-              </Link>
-            </List>
-          </AccordionBody>
-        </Accordion>
+            </ListItem>
+          </Link>
+        ))}
       </List>
     </Card>
   );
