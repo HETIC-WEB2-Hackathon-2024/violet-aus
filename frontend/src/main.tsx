49f44e5bb25fad91fb23c321dc4f7775c2503201
Valentin Machefaux <valentin.machefaux@gmail.com>
2024-06-19 20:00:01 +0200

    Affichage de données sur le dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
@@ -6,20 +5,18 @@ import { Authenticated } from "./auth/Authenticated.tsx";
 import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="violet-aus.eu.auth0.com"
-      clientId="aJ7zv8SnWKNhg3y8NWVNitUPOhdTHg6Q"
-      authorizationParams={{
-        audience: "api.violet.aus.floless.fr",
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <Authenticated>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </Authenticated>
-    </Auth0Provider>
-  </React.StrictMode>
+  <Auth0Provider
+    domain="violet-aus.eu.auth0.com"
+    clientId="aJ7zv8SnWKNhg3y8NWVNitUPOhdTHg6Q"
+    authorizationParams={{
+      audience: "api.violet.aus.floless.fr",
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <Authenticated>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Authenticated>
+  </Auth0Provider>
 );
