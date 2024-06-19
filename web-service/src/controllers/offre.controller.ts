f220a9d25a0b7f3f2d7d622f71b985d92acc357b
Joachim Renevier <renevierjoachim@gmail.com>
2024-06-19 09:47:06 +0200

    fix(extend): comment find extend

--- web-service/src/controllers/offre.controller.ts
+++ web-service/src/controllers/offre.controller.ts
@@ -98,12 +98,17 @@ export const updateById = async (req: Request, res: Response) => {
 // export const getByIdExtend = async (req: Request, res: Response) => {
 //   try {
 //     const id = parseInt(req.params.id, 10);
+// export const getByIdExtend = async (req: Request, res: Response) => {
+//   try {
+//     const id = parseInt(req.params.id, 10);
 
+//     const result = await offreRepository.findByIdExtend(id);
+//     const offre: Offre = new Offre(result.rows[0])
 //     const result = await offreRepository.findByIdExtend(id);
 //     const offre: Offre = new Offre(result.rows[0])
 
 //     res.status(200).json({ message: 'Offre retrieved successfully', offre: offre });
-
+  
 //   } catch (error: any) {
 //     res.status(500).json({ message: 'Internal Server Error', error: error.message });
 //   }
--- web-service/src/routes/offre.route.ts
+++ web-service/src/routes/offre.route.ts
@@ -1,21 +1,16 @@
 import { Router } from "express";
 
-import createOffreDto from "./dto/offre/createOffre.dto";
-import updateOffreDto from "./dto/offre/updateOffre.dto";
-import validator from "../middlewares/validator.middleware";
-import {
-  getById,
-  getAll,
-  createOne,
-  create,
-  deleteById,
-  updateById,
-} from "../controllers/offre.controller";
+import createOffreDto from './dto/offre/createOffre.dto';
+import updateOffreDto from './dto/offre/updateOffre.dto';
+import validator from '../middlewares/validator.middleware'
+import { getById , getAll, createOne, create, deleteById, updateById} from '../controllers/offre.controller';
 
 const router = Router();
 
-router.get('/:id', getById)
-router.get('/', getAll);;
+router.post('/many', create);
+// router.get('/extend/:id', getByIdExtend);
+router.get('/:id', getById);
+router.get('/', getAll);
 router.post('/', validator(createOffreDto), createOne);
 
 export default router;
