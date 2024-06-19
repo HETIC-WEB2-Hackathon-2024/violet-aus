13cf07f7eedddb1ed604ff1f36a82982f50aa0ec
heitzjulien <heitzjulien@pm.me>
2024-06-19 09:47:42 +0200

    refactor: update link in Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -15,7 +15,6 @@ const Menus = [
   { title: "Ma sélection", icon: "Bookmark", link: "/Selection" },
   { title: "Paramètres", icon: "Params", link: "/Parametres" },
   { title: "Connexion", icon: "Connexion", link: "/Connexion" },
-
 ];
 
 const icons: { [key: string]: string } = {
@@ -26,13 +25,9 @@ const icons: { [key: string]: string } = {
   Bookmark:
     "M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z",
   Params:
-  "M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z",
-  Connexion:
-  "M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9",
-
+    "M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z",
+  Connexion: "M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9",
 };
-
-
 
 export default function Header() {
   const [sidenavOpen, setSidenavOpen] = React.useState(true);
@@ -42,102 +37,97 @@ export default function Header() {
         sidenavOpen ? "w-72" : "w-20 "
       } relative duration-100 h-100vh p-1 shadow-xl bg-primary-base rounded-none`}
     >
-      <Link to="/">
-        <ListItemPrefix className="text-secondary-light">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className={`absolute cursor-pointer -right-3 top-9 w-7 border-secondary-light
+      <ListItemPrefix className="text-secondary-light">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-secondary-light
                          border-2 rounded-full size-6  ${
                            !sidenavOpen && "rotate-180"
                          }`}
-            onClick={() => setSidenavOpen(!sidenavOpen)}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5 8.25 12l7.5-7.5"
-            />
-          </svg>
-        </ListItemPrefix>
-        <div className="flex flex-col items-center gap-2 p-4">
-          <img
-            src="/logo-aus.svg"
-            className={`cursor-pointer duration-500 ${
-              sidenavOpen && "rotate-[360deg]"
-            }`}
+          onClick={() => setSidenavOpen(!sidenavOpen)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
           />
-        </div>
-      </Link>
+        </svg>
+      </ListItemPrefix>
+
+      <div className="flex flex-col items-center gap-2 p-4">
+        <img
+          src="/logo-aus.svg"
+          className={`cursor-pointer duration-500 ${
+            sidenavOpen && "rotate-[360deg]"
+          }`}
+        />
+      </div>
+
       <hr className=" border-primary-dark" />
-      { sidenavOpen ?
-      <List>
-      {Menus.map((menu, index) => (
-        <Link key={index} to={menu.link}>
-          <Button
-            className="flex items-center gap-3 w-full bg-primary-base hover:bg-primary-dark focus:bg-primary-light shadow-none"
-          >
-            <ListItemPrefix className="text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d={icons[menu.icon]}
-                />
-              </svg>
-            </ListItemPrefix>
-            <Typography
-              variant="h5"
-              color="white"
-              className={`${
-                !sidenavOpen && "hidden"
-              } origin-left duration-200 ml-2`}
-            >
-              {menu.title}
-            </Typography>
-          </Button>
-        </Link>
-      ))}
-    </List>
-      : 
-      <List className="gap-2">
-      {Menus.map((menu, index) => (
-          <IconButton
-            className="flex items-center w-full bg-primary-base hover:bg-primary-dark focus:bg-primary-light ml-2 shadow-none"
-          >
+      {sidenavOpen ? (
+        <List>
+          {Menus.map((menu, index) => (
             <Link key={index} to={menu.link}>
-              <ListItemPrefix className="text-white mr-0">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
+              <Button className="flex items-center gap-3 w-full bg-primary-base hover:bg-primary-dark focus:bg-primary-light shadow-none">
+                <ListItemPrefix className="text-white">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d={icons[menu.icon]}
+                    />
+                  </svg>
+                </ListItemPrefix>
+                <Typography
+                  variant="h5"
+                  color="white"
+                  className={`${
+                    !sidenavOpen && "hidden"
+                  } origin-left duration-200 ml-2`}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d={icons[menu.icon]}
-                  />
-                </svg>
-              </ListItemPrefix>
-              </Link>
-          </IconButton>
-      ))}
-    </List>
-      }
-    
+                  {menu.title}
+                </Typography>
+              </Button>
+            </Link>
+          ))}
+        </List>
+      ) : (
+        <List className="gap-2">
+          {Menus.map((menu, index) => (
+            <Link key={index} to={menu.link}>
+              <IconButton className="flex items-center w-full bg-primary-base hover:bg-primary-dark focus:bg-primary-light ml-2 shadow-none">
+                <ListItemPrefix className="text-white mr-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d={icons[menu.icon]}
+                    />
+                  </svg>
+                </ListItemPrefix>
+              </IconButton>
+            </Link>
+          ))}
+        </List>
+      )}
     </Card>
   );
 }
