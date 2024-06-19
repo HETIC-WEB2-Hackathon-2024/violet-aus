d6cd3f42816d74cef6fa4248d42bf0119ae6b55d
GDKobrae <djedjegboble.pro@gmail.com>
2024-06-19 21:10:13 +0200

    feat: add onChange function props

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,8 +1,23 @@
 interface SearchProps {
   placeholder: string;
   className: string;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function SearchBar({ placeholder, className }: SearchProps) {
-  return <input type="text" placeholder={placeholder} className={className} />;
+export default function SearchBar({
+  placeholder,
+  className,
+  name,
+  onChange,
+}: SearchProps) {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      className={className}
+      name={name}
+      onChange={(event) => onChange(event)}
+    />
+  );
 }
