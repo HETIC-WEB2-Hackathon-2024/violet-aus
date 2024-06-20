bcc63f972383ad75e1938a0a076af9e7d4c3dd9b
LinelinLove <chrisline.lin@outlook.fr>
2024-06-20 10:53:52 +0200

    modified:   frontend/src/App.tsx 	modified:   frontend/src/pages/Dashboard.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       //   element: <Selection />,
       // },
       {
-        path: "Settings",
+        path: "parametres",
         element: <Settings />,
       },
       // {
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 import { authenticatedGet } from "../auth/helper";
-import { Skeleton } from "@mui/material";
+// import { Skeleton } from "@mui/material";
 
 const Dashboard = () => {
   const { getAccessTokenSilently } = useAuth0();
@@ -23,20 +23,21 @@ const Dashboard = () => {
     }
     callApi();
   }, []);
-  
-  let content = <div>Page not found</div>
-  if(loading){
-    content = <Skeleton variant="rounded" animation="wave" width="100%" height="100%"/>
-  } else {
-    content = <div>else</div>
-  }
 
+  let content = <div>Page not found</div>;
+  if (loading) {
+    content = (
+      <Skeleton variant="rounded" animation="wave" width="100%" height="100%" />
+    );
+  } else {
+    content = <div>else</div>;
+  }
 
   return (
     <section className="flex justify-center items-center h-full w-full">
       {content}
     </section>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
