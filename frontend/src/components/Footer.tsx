9b9b0c0d4d0c277bab44cb9b35920afb8ae5877a
heitzjulien <heitzjulien@pm.me>
2024-06-20 15:44:52 +0200

    chore: Update footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import { Typography } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
     <footer className="w-full bg-gray-light dark:bg-gray-darkest p-4">
     <Typography color="blue-gray" className="text-center font-normal text-gray-lightest">
-        &copy; 2024 Adopte un stagiaire
+        &copy; 2024 Adopte un <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ">stagiaire</Link>
       </Typography>
     </footer>
     )
