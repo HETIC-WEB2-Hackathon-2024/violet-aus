ce9f85888d89669e618ea3cd5c3c80e7dd4c04c1
GDKobrae <djedjegboble.pro@gmail.com>
2024-06-20 18:31:59 +0200

    chore: remove commentary + prettier

--- web-service/src/repositories/helper.repository.ts
+++ web-service/src/repositories/helper.repository.ts
@@ -36,79 +36,38 @@ export default class HelperRepository {
     return result;
   }
 
-
-
-  // async findByIdExtend(id: number): Promise<any> {
-
-  //   const join = this.fKey.map((foreignKey) => `LEFT JOIN ${foreignKey.reference} ON ${foreignKey.reference}.id = ${this.entity}.${foreignKey.key}')' `).join(' ')
-
-  // this.fKey.map((foreignKey) => {
-  //   values += `, JSON_BUILD_OBJECT(*) AS ${foreignKey.reference}`
-  // })
-
-  //     const values = this.fKey.map((foreignKey) => {
-  //       foreignKey.map((column)=>{ `${foreignKey.reference}
-
-  //       })
-
-  //     }
-  //       `, JSON_BUILD_OBJECT(${foreignKey.reference}.id, ${foreignKey.reference}.id, ${foreignKey.reference}.name, ${foreignKey.reference}.name) AS ${foreignKey.reference}`).join(' ');
-
-  //     console.log (join)
-  //     console.log (values)
-  //     const sql = `SELECT * ${values} FROM ${this.entity} ${join} WHERE id = $1`;
-
-  //     const result = await query(sql, [id]);
-  //     return result.rows[0];
-  // }
-
   async findAll(): Promise<any> {
-    // TO REMOVE: I set the limit for avoid to crash my localhost
     const sql = `SELECT * FROM ${this.entity} LIMIT 10`;
     const result = await query(sql);
     return result;
   }
 
-  // async findByOption(entity: string, option: Option): Promise<any> {
-  //   let queryString = `SELECT * FROM ${entity} WHERE `;
-  //   const filterKeys = Object.keys(option.filter);
-  //   filterKeys.forEach((key, index) => {
-  //     queryString += `${key} = '${option.filter[key]}'`;
-  //     if (index < filterKeys.length - 1) {
-  //       queryString += ' AND ';
-  //     }
-  //   });
-  //   return query(queryString);
-  // }
-
-
   async createOne(data: any): Promise<any> {
-    const keys:string = Object.keys(data).join(', ');
-    const valuesArray  = Object.values(data)
-    const values:string = valuesArray.map((value) => `'${value}'`).join(', ');
+    const keys: string = Object.keys(data).join(", ");
+    const valuesArray = Object.values(data);
+    const values: string = valuesArray.map((value) => `'${value}'`).join(", ");
 
     const sql = `INSERT INTO ${this.entity} (${keys}) VALUES (${values}) RETURNING *`;
-  
+
     const result = await query(sql);
     return result;
   }
 
   async create(datas: [any]): Promise<any> {
-    const keys:string = Object.keys(datas[0]).join(', ');
+    const keys: string = Object.keys(datas[0]).join(", ");
 
-    let values = '';
+    let values = "";
     datas.forEach((data, index) => {
-
-      const valuesArray  = Object.values(data)
-      values += '(' + valuesArray.map((value) => `'${value}'`) +')';
+      const valuesArray = Object.values(data);
+      values += "(" + valuesArray.map((value) => `'${value}'`) + ")";
 
       if (index < datas.length - 1) {
-        values += ', ';
+        values += ", ";
       }
     });
 
     const sql = `INSERT INTO ${this.entity} (${keys}) VALUES ${values} RETURNING *`;
-  
+
     const result = await query(sql);
     return result;
   }
@@ -121,13 +80,13 @@ export default class HelperRepository {
 
   async updateById(id: number, data: any): Promise<any> {
     const keys = Object.keys(data);
-    let values = '';
+    let values = "";
 
     keys.forEach((key, index) => {
       values += `${key} = '${data[key]}'`;
 
       if (index < keys.length - 1) {
-        values += ', ';
+        values += ", ";
       }
     });
 
