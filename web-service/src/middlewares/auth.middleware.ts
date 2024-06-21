dd1e14713bfbc32e545798a6a106f3a0bbd79147
Joachim Renevier <renevierjoachim@gmail.com>
2024-06-21 10:43:22 +0200

    wip

--- web-service/src/middlewares/auth.middleware.ts
+++ web-service/src/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { auth } from 'express-oauth2-jwt-bearer';
 import { Request, Response, NextFunction } from 'express';
+
 const jwtCheck = auth({
   audience: 'api.violet.aus.floless.fr',
   issuerBaseURL: 'https://violet-aus.eu.auth0.com/',
@@ -12,38 +13,9 @@ const authMiddleware =  async (req: Request, res: Response, next: NextFunction) 
     if (err) {
       return res.status(401).json({ error: 'Unauthorized', message: 'Invalid or missing token' });
     }
-
-    try {
-      const userinfoResponse = await fetch(
-        'https://violet-aus.eu.auth0.com/userinfo',
-        {
-          headers: {
-            Authorization: `Bearer ${req.auth?.token}`,
-          },
-        }
-      );
-
-      if (!userinfoResponse.ok) {
-        return res.status(401).json({ error: 'Unauthorized', message: 'Failed to fetch user info' });
-      }
-
-      const userinfo = await userinfoResponse.json();
-      const user = await candidatRepository.findByEmail(userinfo.email)
-
-      req.user = {
-        ...user.rows[0],
-        login: userinfo.name,
-        picture: userinfo.picture, 
-        updated_at: userinfo.updated_at,
-        email_verified: userinfo.email_verified,
-      };
       
-      next();
+    next();
 
-    } catch (error) {
-      console.error('Error fetching user info:', error);
-      return res.status(500).json({ error: 'Internal Server Error', message: 'Failed to fetch user info' });
-    }
   });
 };
 export default authMiddleware;
